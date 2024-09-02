27becbb71ffcdba4320eb6b66553532e52502e82
mohith1490 <mohith1754singh@gmail.com>
2024-09-02 20:01:22 +0530

    enhanced-code

--- userTransactions.js
+++ userTransactions.js
@@ -9,18 +9,25 @@ export async function userTransactions(req, res) {
         let etherscan_url = `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=${process.env.ETHERSCAN_API_KEY}`
         const response = await fetch(etherscan_url); //fetching data from provided address
         let transition_history = await response.json();
+
+
         // checking if message is ok else return error
         if (transition_history.message !== "OK") {
             return res.send(400).json({ message: "error occured enter a valid address" });
         }
+
+
         let totalexpenses = 0; //calculating total expenses of user and storing in model for longterm purposes
         transition_history.result.forEach(element => {
             let result = (element.gasPrice * element.gasUsed) / Math.exp(18);
             totalexpenses += result;
         });
+
+        
         // returning array of transactions and storing address to easily access in future need 
         const transaction = transition_history.result.map((payment) => ({ ...payment, address, totalexpenses }));
         await transactionModel.insertMany(transaction);
+
     } catch (error) {
         console.error("error occured")
     }
